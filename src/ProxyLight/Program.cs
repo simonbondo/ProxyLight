dcbaafd592fac7e5dc2d6ecebafffb9211a71656
Simon Bondo <simonbondo@users.noreply.github.com>
2025-07-07 17:46:51 +0200

    Implement super simple proxy

--- src/ProxyLight/Program.cs
+++ src/ProxyLight/Program.cs
@@ -1,6 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Mvc;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddCors()
+    .AddHttpClient("ProxyClient", client =>
+    {
+        client.Timeout = TimeSpan.FromSeconds(30);
+    });
+
+var app = builder.Build();
+app.UseCors(policy => policy
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.MapGet("/", async (HttpClient proxy, CancellationToken token, [FromQuery(Name = "u")] string remoteUrl) =>
+{
+    try
+    {
+        var response = await proxy.GetAsync(remoteUrl, token);
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsByteArrayAsync(token);
+        return Results.Bytes(content, response.Content.Headers.ContentType?.ToString() ?? "application/octet-stream");
+    }
+    catch (HttpRequestException e)
+    {
+        return Results.Problem(e.Message);
+    }
+});
 
 app.Run();
